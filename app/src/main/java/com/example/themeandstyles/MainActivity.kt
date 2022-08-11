60aae9be8d944ad7105ac0af420096a5e5035992
npogoncuk <npogoncuk@gmail.com>
2022-08-11 18:06:44 +0300

    Created a button, which changes AppTheme using values-night

--- app/src/main/java/com/example/themeandstyles/MainActivity.kt
+++ app/src/main/java/com/example/themeandstyles/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.themeandstyles
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
+import com.example.themeandstyles.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private var isNightMode = AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.changeModeButton.setOnClickListener {
+            if (isNightMode) AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            else AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            isNightMode = !isNightMode
+        }
     }
 }
